b9a6d35714d6af19ccc0eb3f2530113e38ebe4e7
Evyatar Ifrach <evyatarevi@gmail.com>
2024-07-24 13:34:14 +0300

    teacher/exams&videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"; //
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"; //
+import Grades from "./Pages/Teacher/Grades";
+import ClassesList from "./Pages/Teacher/ClassesList";
+import ClassPage from "./Pages/Teacher/ClassPage";
 
 const AppLayout = () => {
   return (
     <div className="h-full w-full flex flex-row gap-8 text-red-700">
+      {/* <Grades /> */}
       {/* SideBar */}
       {/* Navbar */}
+      <Outlet />
     </div>
   );
 };
@@ -14,6 +19,14 @@ const router = createBrowserRouter([
     path: "/",
     element: <AppLayout />,
     children: [
+      {
+        path: "/class",
+        element: <ClassesList />,
+      },
+      {
+        path: "class/:id",
+        element: <ClassPage />,
+      },
       // {
       //   index: true,
       //   path: "/",
