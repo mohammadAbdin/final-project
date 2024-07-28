919385e9fd00d7e0ffc696eab567fdbca613b0c0
Evyatar Ifrach <evyatarevi@gmail.com>
2024-07-28 12:29:47 +0300

    m

--- src/Pages/Teacher/ClassPage.tsx
+++ src/Pages/Teacher/ClassPage.tsx
@@ -33,17 +33,17 @@ const fetchTopics: Topic[] = [
       {
         id: 1,
         title: "video1",
-        url: "sdfsdfsdf",
+        url: "https://www.youtube.com/embed/jhiY62jG45o",
       },
       {
         id: 2,
         title: "video2",
-        url: "sdfsdfsdf",
+        url: "https://www.youtube.com/embed/jhiY62jG45o",
       },
       {
         id: 3,
         title: "video3",
-        url: "sdfsdfsdf",
+        url: "https://www.youtube.com/embed/jhiY62jG45o",
       },
     ],
   },
@@ -54,17 +54,17 @@ const fetchTopics: Topic[] = [
       {
         id: 1,
         title: "video1",
-        url: "sdfsdfsdf",
+        url: "https://www.youtube.com/embed/jhiY62jG45o",
       },
       {
         id: 2,
         title: "video2",
-        url: "sdfsdfsdf",
+        url: "https://www.youtube.com/embed/jhiY62jG45o",
       },
       {
         id: 3,
         title: "video3",
-        url: "sdfsdfsdf",
+        url: "https://www.youtube.com/embed/jhiY62jG45o",
       },
     ],
   },
@@ -212,7 +212,7 @@ const ClassPage: React.FC = () => {
                   </div>
                 </div>
                 {expandedTopicId === topic.id && (
-                  <div className="">
+                  <div>
                     {topic.videos.map((video) => (
                       <div
                         key={video.id}
@@ -220,18 +220,25 @@ const ClassPage: React.FC = () => {
                       >
                         <div>
                           <p className="font-semibold">{video.title}</p>
-                          <a
-                            href={video.url}
-                            className="text-blue-500 hover:underline"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            Watch Video
-                          </a>
+
+                          <div className="w-1/2 aspect-square">
+                            <iframe
+                              src={video.url}
+                              title="YouTube video player"
+                              frameBorder="0"
+                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                              allowFullScreen
+                              className="w-full h-full"
+                            />
+                          </div>
                         </div>
+                        <p>
+                          Descripuon: Lorem ipsum dolor sit amet consectetur
+                          adipisicing elit. Assumenda, neque.
+                        </p>
                         <div>
                           <button
-                            className="px-2 py-1 text-sm text-white bg-blue-500 rounded mr-2"
+                            className="px-2 py-1 text-sm text-white bg-purple-500 rounded mr-2 mb-2"
                             onClick={() => editVideo(video.id)}
                           >
                             Edit
@@ -245,12 +252,14 @@ const ClassPage: React.FC = () => {
                         </div>
                       </div>
                     ))}
-                    <button
-                      className="px-4 py-2 bg-green-500 text-white rounded"
-                      onClick={addVideo}
-                    >
-                      Add Video
-                    </button>
+                    <div className="flex justify-center items-center my-3">
+                      <button
+                        className="px-4 py-2 bg-green-500 text-white rounded"
+                        onClick={addVideo}
+                      >
+                        Add Video
+                      </button>
+                    </div>
                   </div>
                 )}
               </li>
