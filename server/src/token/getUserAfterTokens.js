d4c2795acecac0668eda8a08f478aa5a3d1d1f0b
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-07-31 13:37:37 +0300

    adding exam grades done

--- server/src/token/getUserAfterTokens.js
+++ server/src/token/getUserAfterTokens.js
@@ -4,7 +4,6 @@ export const getUserAfterTokens = async (req, res) => {
   try {
     const User = await getUserModel();
     const user = await User.findById(req.user.userId).select("-password");
-    console.log(user);
     res.status(200).json(user);
   } catch (error) {
     console.error("Error fetching user data:", error);
