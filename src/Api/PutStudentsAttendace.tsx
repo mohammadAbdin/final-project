1e727f4ec365e68c3328cbab436a4b190f4970f0
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-08-01 09:40:40 +0300

    finishing attendance and exams

---
+++ src/Api/PutStudentsAttendace.tsx
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { StudentsAttendanceType } from "../Types/StudentsAttendanceType";
+
+export const PutStudentsAttendace = async (
+  studentsWithAttendance: StudentsAttendanceType[] | undefined,
+  selectedDate: string,
+  teacher_id: string | undefined,
+  classNumber: string | undefined
+): Promise<void> => {
+  if (!studentsWithAttendance || !selectedDate || !teacher_id) {
+    console.error("User ID or class number is missing.");
+    return;
+  }
+
+  try {
+    console.log(studentsWithAttendance, selectedDate, teacher_id, classNumber);
+
+    await axios.put("http://localhost:5001/class/attendance-students-edits", {
+      studentsWithAttendance,
+      selectedDate,
+      teacher_id,
+      classNumber,
+    });
+    console.log("Exam marks updated successfully.");
+  } catch (error) {
+    console.error("Failed to update exam marks:", error);
+  }
+};
