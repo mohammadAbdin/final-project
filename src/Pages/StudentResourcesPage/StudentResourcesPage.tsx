e43b69f0f21957712b58d1a9a82c14860f15aa8e
MohammadAbdin <mohamad-abdeen333@hotmail.com>
2024-08-01 11:57:04 +0300

    merging with avi

--- src/Pages/StudentResourcesPage/StudentResourcesPage.tsx
+++ src/Pages/StudentResourcesPage/StudentResourcesPage.tsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import UseGetChildSubjects from "../../Hooks/UseGetChildSubjects";
 import { useParams } from "react-router-dom";
+import { UserContext } from "../../Context/UserContext";
 
 const StudentResourcesPage = () => {
   const { student_id } = useParams<{ student_id: string }>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const { user } = useContext(UserContext);
+
   const { getChildSubjects, childSubjects } = UseGetChildSubjects(
     setIsLoading,
     student_id
   );
   const [subject, setSubject] = useState<string | null>(null);
+  useEffect(() => {
+    if (isLoading && user && !childSubjects) {
+      if (user._id !== undefined) getChildSubjects();
+    }
+    if (childSubjects) {
+      setSubject(subject || childSubjects[0]);
+    }
+  }, [isLoading, user, getChildSubjects, childSubjects, subject]);
+
   return (
     <div>
       <h2>You are watching: {subject}</h2>
