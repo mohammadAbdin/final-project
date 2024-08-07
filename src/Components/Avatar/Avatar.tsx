07467edd20c7fbbbcac8b2834f7c11b34db5fb8a
Avi Israeli <aviv8710@gmail.com>
2024-08-07 15:30:50 +0300

    avatar

--- src/Components/Avatar/Avatar.tsx
+++ src/Components/Avatar/Avatar.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import first from "../../../src/assets/Images/1st-grade-student.png";
+import second from "../../../src/assets/Images/2nd-grade-student.png";
+import third from "../../../src/assets/Images/3rd-grade-student.png";
+import fourth from "../../../src/assets/Images/4rth-grade-student.png";
+import fifth from "../../../src/assets/Images/5th-grade-student.png";
+import sixth from "../../../src/assets/Images/6th-grade-student.png";
 
 interface AvatarProps {
-  classGrade: number; // Represents the student's grade/class
+  classGrade: string; // Represents the student's grade/class
   averageScore: number; // Represents the student's average score
   studentName: string; // Student's name
 }
@@ -14,40 +20,42 @@ const Avatar: React.FC<AvatarProps> = ({
   const getAvatarColor = () => {
     if (averageScore >= 90) return "bg-green-500"; // High score
     if (averageScore >= 75) return "bg-yellow-500"; // Medium score
+    if (averageScore >= 65) return "bg-orange-500"; // Medium-Low score
     return "bg-red-500"; // Low score
   };
 
   const getAvatarIcon = () => {
     switch (classGrade) {
-      case 1:
-        "1st";
-        break;
-      case 2:
-        "2nd";
-        break;
-      case 3:
-        "3rd";
-        return "👶"; // Example icon for lower grades
-      case 4:
-      case 5:
-      case 6:
-        return "🧒"; // Example icon for middle grades
-      case 7:
-      case 8:
-      case 9:
-        return "👦"; // Example icon for upper middle grades
-      case 10:
-      case 11:
-      case 12:
-        return "🧑"; // Example icon for high school
+      case "1":
+      case "2":
+        return first;
+      case "3":
+      case "4":
+        return second;
+      case "5":
+      case "6":
+        return third;
+      case "7":
+      case "8":
+        return fourth;
+      case "9":
+      case "10":
+        return fifth;
+      case "11":
+      case "12":
+        return sixth;
       default:
         return "👤"; // Default icon
     }
   };
 
   return (
-    <div className={`flex items-center ${getAvatarColor()} p-4 rounded-full`}>
-      <span className="text-2xl">{getAvatarIcon()}</span>
+    <div
+      className={`flex items-center ${getAvatarColor()} p-4 rounded-md border-2 border-blue-500`}
+    >
+      <span className="text-2xl">
+        <img src={getAvatarIcon()} alt="" className="w-10 rounded-md" />
+      </span>
       <span className="ml-2 text-white">{studentName}</span>
     </div>
   );
